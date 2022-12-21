ee3f6a28486c3ff2caf2384cfdd6a8b09589a17e
belyakov.va <cock@dick>
2022-12-21 12:12:52 +0300

    last worked

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
 import yaml
+from config import Config
 '''
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,8 +11,9 @@ from config import Config
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-#DB CONFIGURATION
 
+#DB CONFIGURATION
+app.config.from_object(Config)
 db = yaml.load('db.yaml', Loader=yaml.Loader)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -24,8 +26,10 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 '''
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+
     cursor = mysql.connection.cursor()
     cursor.execute("INSERT INTO books (`book_name`, `book_category`) VALUES ('обновил', 'нажал')")
     mysql.connection.commit()
@@ -33,15 +37,36 @@ def index():
     print('СЧЕТЧИК КОЛ-ВА ЗАПИСЕЙ: ', result_value)
     colors = ['red', 'blue', 'green']
 
-    return render_template('index.html', colors=colors)
+    if request.method == 'POST':
+        form = request.form
+        name = form['name']
+        age = form['age']
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO employee(name, age) VALUES(%s, %s)", (name, age))
+        mysql.connection.commit()
+    return render_template('index.html')
 
+ButtonPressed = 0
+
+@app.route('/formworkers', methods=['GET', 'POST'])
+def formworkers():
+
+    cursor = mysql.connection.cursor()
+    cur = cursor.execute("SELECT firstname, lastname FROM persons")
+    return render_template('formworkers.html', workers0=cursor.fetchall(), ButtonPressed=ButtonPressed)
+
+
+@app.route('/process_data/', methods=['POST'])
+def doit():
+    index = request.form['index']
+    print(index)
 
 @app.route('/css')
 def css():
     return render_template('css.html')
 
 
-@app.route('/out')
+@app.route('/out', methods=['GET', 'POST'])
 def out():
     list_ = [1111, 2222, 3333]
     return render_template('out.html', option=list_)
