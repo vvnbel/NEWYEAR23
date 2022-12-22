d822ee08bd950fed5c39483482d1ac8006bef47f
vvnbel <vvnbel@gmail.com>
2022-12-22 07:02:06 +0300

    redesigned the basic template, login page, added an email form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
+    remember_me = BooleanField('Remember Me', default=False)
     submit = SubmitField('Sign In')
